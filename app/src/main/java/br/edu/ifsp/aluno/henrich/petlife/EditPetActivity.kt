b127bae8dd942d3c6b1424fb3cd116ddd3437cb1
Henrich-Caeiro <henrich.caeiro@gmail.com>
2024-10-21 20:03:32 -0300

    create a activity edit pet layout

--- app/src/main/java/br/edu/ifsp/aluno/henrich/petlife/EditPetActivity.kt
+++ app/src/main/java/br/edu/ifsp/aluno/henrich/petlife/EditPetActivity.kt
@@ -1,6 +1,4 @@
 package br.edu.ifsp.aluno.henrich.petlife
-package com.example.petlife
-
 import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -10,15 +8,15 @@ class EditPetActivity : AppCompatActivity() {
 
     private lateinit var pet: Pet
     private lateinit var binding: ActivityEditPetBinding
-
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditPetBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+//
         pet = intent.getSerializableExtra("pet") as Pet
-
-        // Preencher os campos com os dados atuais do pet
+//
+//        // Preencher os campos com os dados atuais do pet
         binding.etPetName.setText(pet.name)
         binding.etPetBirthDate.setText(pet.birthDate)
         binding.etPetType.setText(pet.type)
